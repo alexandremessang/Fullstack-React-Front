712567bf54c9a574c41df09a4f6e79cdfaac220c
Alexandre <alexandre.messang@gmail.com>
2022-01-06 15:57:59 +0100

    fix carousel

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,15 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { Navigation, Scrollbar } from 'swiper';
+import { Navigation } from 'swiper';
 import './styles/homepage.style.css'
 // import Swiper JS
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import Swiper styles
-import "swiper/swiper-bundle.min.css";
+import "swiper/swiper-bundle.min.css"
+import "swiper/swiper.min.css"
+
+const goNext = () => {
+    const swiper = document.querySelector('.swiper').swiper;
+    swiper.slideNext()
+}
+
+const goPrev = () => {
+    const swiper = document.querySelector('.swiper').swiper;
+    swiper.slidePrev()
+}
 
 
 function Homepage(props) {
 
+    
     // Create array with 1000 slides
     const slides = Array.from({ length: 6 }).map(
         (el, index) => <Vignette ></Vignette>
@@ -22,24 +34,28 @@ function Homepage(props) {
                     <h3>Des idées pour votre prochain voyage</h3>
                 </div>
                 <div className="sliderControls">
-                    <div className="slidePrev">
-                        <span>{'<'}</span>
+                    <div className="prevSlide" onClick={goPrev}>
+                        <i class="fas fa-angle-left"></i>
                     </div>
-                    <div className="slideNext">
-                        <span>{'>'}</span>
+                    <div className="nextSlide" onClick={goNext}>
+                        <i class="fas fa-angle-right"></i>
+                        
                     </div>
                 </div>
             </div>
             <Swiper 
-                modules={[Navigation, Scrollbar]} 
-                spaceBetween={20} 
-                slidesPerView={3} 
-                navigation
-                allowTouchMove={true}
+                className='swiper'
+                modules={[]} 
+                spaceBetween={30} 
+                slidesPerView={4} 
+                rewind={true}
+                onSlideChange={() => {
+                    console.log('change');
+                }}
             >
                 {slides.map((slideContent, index) => (
-                    <SwiperSlide key={slideContent} virtualIndex={index}>
-                    {slideContent}
+                    <SwiperSlide key={index}>
+                        {slideContent}
                     </SwiperSlide>
                 ))}
             </Swiper>
